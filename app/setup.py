d7585ce37d98b289c85b9c1073440c0da4bc000e
Chris Cooke <kooshini@gmail.com>
2019-02-14 02:00:33 +0000

    vuln

--- app/setup.py
+++ app/setup.py
@@ -32,7 +32,7 @@ setup(
     platforms=['OS Independent'],
     classifiers=CLASSIFIERS,
     install_requires=[
-        'Django==1.11',
+        'Django==1.11.20',
     ],
     packages=find_packages(exclude=["project","project.*"]),
     include_package_data=True,
